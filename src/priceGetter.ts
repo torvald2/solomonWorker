855a2292f4db2b36692bf5a78e0b29cb27176993
denisskripnik <valis.source@gmail.com>
2021-12-14 08:23:02 +0200

    1

--- src/priceGetter.ts
+++ src/priceGetter.ts
@@ -2,7 +2,7 @@
 import { Cluster, clusterApiUrl, Connection } from '@solana/web3.js'
 import { getPythProgramKeyForCluster, PriceData, Product, PythConnection } from '@pythnetwork/client'
 import {DbClient} from './mongoClient'
-import {TokenList} from './tokenList'
+import {TokenList, Token} from './tokenList'
 export  class PriceGetter{
 	private solanaCluster:Cluster 
 	private pythConnection: PythConnection
@@ -23,11 +23,23 @@ export  class PriceGetter{
 		this.pythConnection.onPriceChange(async(product: Product, price: PriceData) => {
 			const quote = product.symbol.split('/')[0]
 			const tokens =  this.tokenList.GetToken(quote.toLowerCase())
-			if (tokens[0]){
-				if (price.price && price.confidence){
+			if (tokens[0] ){
+				if (price.price && price.confidence &&  !tokens[0].isStock){
 					await this.mongoClient.InsertPrice(tokens[0],price.price, product.symbol)
 				}
-			} 
+			} else {
+				let token:Token = {
+					name:product.symbol,
+					symbol: product.symbol,
+					image: "",
+					address:"",
+					isStock:false
+				}
+				if (price.price && price.confidence){
+					await this.mongoClient.InsertPrice(token,price.price, product.symbol)
+				}
+
+			}
 			
 		})
     
--- src/tokenList.ts
+++ src/tokenList.ts
@@ -1,10 +1,12 @@
 import axios from 'axios';
+import { stringify } from 'querystring';
 
 export interface Token {
     name:string,
     symbol:string,
     image:string,
-	address:string
+	address:string,
+	isStock:boolean,
 }
 
 export  class TokenList{
@@ -15,17 +17,33 @@ export  class TokenList{
 	async LoadData(){
 		const res = await axios.get('https://raw.githubusercontent.com/solana-labs/token-list/main/src/tokens/solana.tokenlist.json')
 		for (const token of res.data.tokens){
+			let isStock = false
+            token.tags.forEach((el:string) => {
+				if (el == "tokenized-stock"){
+					isStock = true
+				}
+				
+			})
 			this.data.push({
 				name:token.name,
 				symbol:token.symbol,
 				image:token.logoURI,
-				address: token.address
+				address: token.address,
+				isStock:isStock
+
 			})
 		}
 	}
 	GetToken(symbol:string){
 		return this.data.filter(x=>{
-			return x.symbol.toLowerCase().indexOf(symbol)!=-1
+			if (x.symbol.toLowerCase().includes(symbol)){
+				return true
+			}
+			if (x.name.toLowerCase().includes(symbol)){
+				return true
+
+			}
+			return false
 		})
 	}
 
